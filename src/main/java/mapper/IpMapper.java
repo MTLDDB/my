28b268de966b1fe9755eda587e408e45fb2100f1
黄畅 <404156918@qq.com>
2019-08-26 17:38:56 +0800

    mybatis

--- src/main/java/mapper/IpMapper.java
+++ src/main/java/mapper/IpMapper.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface IpMapper {
     //void insertIpList(@Param("list") List<IP> list);
     void insertIp(@Param("p") IP p);
+
     IP selectOne(@Param("ip") String ip);
     void update(@Param("p") IP p);
 
